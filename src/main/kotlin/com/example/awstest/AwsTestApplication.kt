c081be52866f7bde0ef303b4d52f308270c86414
wonjeong2 <cwj9342@daum.net>
2023-12-19 11:17:13 +0900

    최초커밋

---
+++ src/main/kotlin/com/example/awstest/AwsTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.awstest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AwsTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<AwsTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/awstest/AwsTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.awstest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AwsTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
